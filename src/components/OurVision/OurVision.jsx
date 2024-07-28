f204137db436d964459df1efda42442a7d2a0743
K Briaan <kbriaan1@gmail.com>
2024-07-28 13:58:12 +0300

    done with mission and vision

--- src/components/OurVision/OurVision.jsx
+++ src/components/OurVision/OurVision.jsx
@@ -1,25 +1,61 @@
+import { motion } from "framer-motion";
+const fadeAnimation = (delay) => {
+  return {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+        delay: delay,
+        ease: "easeInOut",
+      },
+    },
+  };
+};
+
 const OurVision = () => {
   return (
     <div className="container my-16 bg-white dark:bg-gray-900 dark:text-white">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
-        <div className="mx-5 md:mx-8 px-6 py-10 space-y-4 shadow-lg border-t-8 border-green-800 dark:bg-slate-800">
+        <motion.div
+          variants={fadeAnimation(0.2)}
+          initial="initial"
+          whileInView="animate"
+          className="mx-5 md:mx-8 px-6 py-10 space-y-4 shadow-lg border-t-8 border-green-800 dark:bg-slate-800"
+        >
           <h1 className="text-xl font-semibold">Our Vision</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugit id
             sapiente quae, excepturi tempore vel tempora fugiat reprehenderit
             qui.
           </p>
-          <a href="#">Learn More</a>
-        </div>
+          <a
+            href="#"
+            className="inline-block hover:text-primary border-b-2 hover:border-primary"
+          >
+            Learn More
+          </a>
+        </motion.div>
 
-        <div className="mx-5 md:mx-8 px-6 py-10 space-y-4 shadow-lg border-t-8 border-yellow-500 dark:bg-slate-800">
+        <motion.div  variants={fadeAnimation(0.4)}
+          initial="initial"
+          whileInView="animate" className="mx-5 md:mx-8 px-6 py-10 space-y-4 shadow-lg border-t-8 border-yellow-500 dark:bg-slate-800">
           <h1 className="text-xl font-semibold">Our Mision</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugit id
             sapiente quae, excepturi tempore vel tempora fugiat reprehenderit
             qui.
           </p>
-        </div>
+          <a
+            href="#"
+            className="inline-block hover:text-primary border-b-2 hover:border-primary"
+          >
+            Learn More
+          </a>
+        </motion.div>
       </div>
     </div>
   );
