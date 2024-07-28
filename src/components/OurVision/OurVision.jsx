e50417493384eaf05dc7474322d82d46bcf824f4
K Briaan <kbriaan1@gmail.com>
2024-07-28 13:38:12 +0300

    Mission

--- src/components/OurVision/OurVision.jsx
+++ src/components/OurVision/OurVision.jsx
@@ -1,15 +1,28 @@
-
 const OurVision = () => {
   return (
     <div className="container my-16 bg-white dark:bg-gray-900 dark:text-white">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10"> 
-            <div className="mx-5 md:mx-8 px-6 py-10 space-y-4 shadow-lg border-t-8 border-gray-800">
-                <h1 className="text-xl font-semibold">Our Vision</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugit id sapiente quae, excepturi tempore vel tempora fugiat reprehenderit qui.</p>
-            </div>
-             </div>
-    </div>
-  )
-}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+        <div className="mx-5 md:mx-8 px-6 py-10 space-y-4 shadow-lg border-t-8 border-green-800 dark:bg-slate-800">
+          <h1 className="text-xl font-semibold">Our Vision</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugit id
+            sapiente quae, excepturi tempore vel tempora fugiat reprehenderit
+            qui.
+          </p>
+          <a href="#">Learn More</a>
+        </div>
 
-export default OurVision
+        <div className="mx-5 md:mx-8 px-6 py-10 space-y-4 shadow-lg border-t-8 border-yellow-500 dark:bg-slate-800">
+          <h1 className="text-xl font-semibold">Our Mision</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugit id
+            sapiente quae, excepturi tempore vel tempora fugiat reprehenderit
+            qui.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurVision;
