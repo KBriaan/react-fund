8ce17c5db580b137a73f6c4766f61158a16a8f73
K Briaan <kbriaan1@gmail.com>
2024-07-28 12:59:17 +0300

    Modification on the counter

--- src/components/OverviewCounter/OverviewCounter.jsx
+++ src/components/OverviewCounter/OverviewCounter.jsx
@@ -8,7 +8,7 @@ const OverviewCounter = () => {
       transition={{ type: "spring", stiffness: 100 }}
       className="container h-12 md:h-32 p-3"
     >
-      <div>
+      <div className="mx-auto my-4 grid w-full grid-cols-4 divide-x divide-slate-700 bg-white p-2 shadow-lg dark:bg-gray-800 dark:text-white/70 md:max-w-[800px] lg:-translate-y-20 md:p-8 ">
         <div className="flex flex-col items-center justify-center">
           <p className="text-sm font-bold text-black/80 dark:text-white sm:text-lg md:text-3xl">
             
@@ -21,7 +21,22 @@ const OverviewCounter = () => {
               duration={2.75}
             />
           </p>
-          <p className="sm:text-md text-xs md:text-lg">Fundraisers</p>
+          <p className="sm:text-md text-xs md:text-lg dark:text-white">Fundraisers</p>
+        </div>
+        
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-sm font-bold text-black/80 dark:text-white sm:text-lg md:text-3xl">
+            
+            <CountUp
+              enableScrollSpy={true}
+              scrollSpyOnce={true}
+              start={0}
+              end={56}
+              suffix="+"
+              duration={2.75}
+            />
+          </p>
+          <p className="sm:text-md text-xs md:text-lg dark:text-white">Raised</p>
         </div>
       </div>
     </motion.section>
