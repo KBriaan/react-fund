067facf9ae7bf897503300a50ac6e409f0925274
K Briaan <kbriaan1@gmail.com>
2024-07-28 13:29:34 +0300

    media queries

--- src/components/OurVision/OurVision.jsx
+++ src/components/OurVision/OurVision.jsx
@@ -3,7 +3,7 @@ const OurVision = () => {
   return (
     <div className="container my-16 bg-white dark:bg-gray-900 dark:text-white">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10"> 
-            <div className="mx-5">
+            <div className="mx-5 md:mx-8">
                 <h1 className="text-xl font-semibold">Our Vision</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugit id sapiente quae, excepturi tempore vel tempora fugiat reprehenderit qui.</p>
             </div>
