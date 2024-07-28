2457a7d41839fbb065ad11fd8a26f72b7eccc8b0
K Briaan <kbriaan1@gmail.com>
2024-07-28 16:31:03 +0300

    img

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,21 @@
-
+import Image1 from "../../assets/don2.jpg"
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className="bg-slate-100 dark:bg-slate-900 dark:text-white">
+        <div className="container md:h-[500px] flex items-center justify-center py-10">
+            <div className="grid grid-cols-1 items-center gap-4 sm:grid-cols-2">
+                {/* img container */}
+            <div>
+                {/* <img src={Image1} alt="" /> */}
+                <img className="max-w-full h-auto" src={Image1} alt="" />
+
+            </div>
+
+                {/* text container */}
+
+            </div>
+        </div>
+    </div>
   )
 }
 
